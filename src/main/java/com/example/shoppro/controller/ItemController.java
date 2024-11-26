13321442a9f7bf6f5774da26c13f6d95e90eb1c1
aaaa <aaaa@a.a>
2024-11-26 13:20:51 +0900

    item 이미지 관련 컨트롤러 및 html 등 수정

--- src/main/java/com/example/shoppro/controller/ItemController.java
+++ src/main/java/com/example/shoppro/controller/ItemController.java
@@ -43,7 +43,7 @@ public class ItemController {
     }
     @PostMapping("/admin/item/new")
     public String itemFormPost(@Valid ItemDTO itemDTO, BindingResult bindingResult,
-                               List<MultipartFile> multipartFile, Model model){
+                               List<MultipartFile> multipartFile ,Model model){
         //들어오는값 확인
         log.info("들어오는값 확인" + itemDTO);
 
@@ -116,7 +116,7 @@ public class ItemController {
 //        model.addAttribute("list", itemService.list());
 //      principal  로그인시 세션에 등록된 이름 우리는 email
         PageResponseDTO<ItemDTO> pageResponseDTO =
-        itemService.list(pageRequestDTO, principal.getName());
+                itemService.list(pageRequestDTO, principal.getName());
 
         model.addAttribute("pageResponseDTO", pageResponseDTO);
 
@@ -130,7 +130,7 @@ public class ItemController {
                                  Model model, Principal principal){
 
 
-        //기존 read는 email을 확인하지 않았다. 
+        //기존 read는 email을 확인하지 않았다.
         //관리자는 자신의 글만 봐야함으로 자신의 상품을 검색하는 쿼리는 추가하자
         // 1 검색하고 값을가지고 확인하고 다시 맞다면 list , 만들기 쉽다
         // 2 검색부터 자신의 값을 가져오자  , 정확하다?
@@ -151,7 +151,24 @@ public class ItemController {
             return "redirect:/admin/item/list";
         }
 
+    }
 
+
+    @PostMapping("/admin/item/update")
+    public String itemupdate(@Valid ItemDTO itemDTO, BindingResult bindingResult, List<MultipartFile> multipartFiles,   Integer[] delino, Long mainino) {
+
+        if (bindingResult.hasErrors()){
+            log.info("유효성검사 에러");
+            log.info(bindingResult.getAllErrors()); //확인된 모든 에러 콘솔창 출력
+
+
+            return "/item/update";        //다시 이전 페이지
+        }
+
+        itemService.update(itemDTO, itemDTO.getId(), multipartFiles, delino, mainino);
+
+
+        return null;
     }
 
 
