a0800e6c730bfb10598e298b2eaee8f4a91939d4
aaaa <aaaa@a.a>
2024-11-28 14:39:16 +0900

    Order 수정(주문시 수량 차감 등)

---
+++ src/main/java/com/example/shoppro/repository/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.example.shoppro.repository;
+
+import com.example.shoppro.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+
+}
